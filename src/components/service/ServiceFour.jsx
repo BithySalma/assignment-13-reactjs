29ff20e35849370d0161a5e681f71ce9a375dc72
Salma Aktar Bithy <76905965+BithySalma@users.noreply.github.com>
2023-11-08 10:24:07 +0600

    updated

---
+++ src/components/service/ServiceFour.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ServiceFour = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ServiceFour
---
+++ src/components/service/ServiceTwo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ServiceTwo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ServiceTwo
